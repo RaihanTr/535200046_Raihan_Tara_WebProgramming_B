3e582c25887da336147f13dd8aaa8f1492228ed2
RaihanTr <88637736+RaihanTr@users.noreply.github.com>
2021-10-19 22:18:52 +0700

    Add files via upload

---
+++ TUGAS8_RAIHANTARA/05 Routing/0504 ServingWithRouterFile.js
@@ -0,0 +1,38 @@
+const port = 3000,
+  http = require("http"),
+  httpStatusCodes = require("http-status-codes"),
+  router = require("./router"),
+  fs = require("fs"),
+  plainTextContentType = {
+    "Content-Type": "text/plain",
+  },
+  htmlContentType = {
+    "Content-Type": "text/html",
+  },
+  customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+      if (errors) {
+        console.log("Error reading the file...");
+      }
+      res.end(data);
+    });
+  };
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("INDEX");
+});
+
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("POSTED");
+});
+
+http.createServer(router.handle).listen(3000);
+
+console.log(`The Server is listening on port number: ${port}`);
